fa1ac1887eba0cc74342579cf03a8f93f2977653
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-09-13 11:14:39 +0530

    Moved Docker builder to separate folder

--- builder/scratch_builder.sh
+++ builder/scratch_builder.sh
@@ -2,7 +2,7 @@
 
 TEMP_CONTAINER_NAME=hasher-builder-`git rev-parse --short --verify HEAD`
 BRANCH=$1
-docker build -t tchaudhry/rainbow-hasher-go:scratch-builder -f Dockerfile.full .
+docker build -t tchaudhry/rainbow-hasher-go:scratch-builder -f Dockerfile.full ..
 docker run --name=$TEMP_CONTAINER_NAME tchaudhry/rainbow-hasher-go:scratch-builder 
 docker cp $TEMP_CONTAINER_NAME:/app/bin ./
 docker container rm $TEMP_CONTAINER_NAME
